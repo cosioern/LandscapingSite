0151e3dd8a3fed01a9eabd554f90ec05bcfe2069
cosio <cosio@wisc.edu>
2026-05-21 14:03:45 -0500

    New Backend / Favicon

---
+++ landscaping-web/src/main/java/com/SantaAna/landscaping_web/LandscapingWebApplication.java
@@ -0,0 +1,13 @@
+package com.SantaAna.landscaping_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandscapingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandscapingWebApplication.class, args);
+	}
+
+}
--- landscaping-web/src/test/java/com/SantaAna/landscaping_web/LandscapingWebApplicationTests.java
+++ landscaping-web/src/test/java/com/SantaAna/landscaping_web/LandscapingWebApplicationTests.java
@@ -1,10 +1,10 @@
-package com.landscaping.backend;
+package com.SantaAna.landscaping_web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class LandscapingWebApplicationTests {
 
 	@Test
 	void contextLoads() {
